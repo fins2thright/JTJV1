1aacd51f0cb3c6a73331c6e6367ab7d912d49f6e
fins2thright <waltsmith@embarqmail.com>
2021-09-15 12:44:39 -0400

    fixed flashlight grab

--- Assets/Scripts/FlashlightBehaviors.cs
+++ Assets/Scripts/FlashlightBehaviors.cs
@@ -4,24 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(Flashlight))]
+
 public class FlashlightBehaviors : GrabbableEvents
 {
-    public Flashlight flashlight;
-
+   
     public override void OnGrab(Grabber grabber)
     {
+       
         Debug.Log("Flashlight Picked Up");
-        flashlight = GetComponent<Flashlight>();
-        flashlight.SpotLight.intensity = 2.1f;
+        GameObject flashlight = GameObject.Find("/Flashlight/WhiteLight");
+        Light light = flashlight.GetComponent(typeof(Light)) as Light;
+        light.intensity = 2.1f;
         base.OnGrab(grabber);
     }
 
     public override void OnRelease()
     {
+        
         Debug.Log("Flashlight Released");
-        flashlight = GetComponent<Flashlight>();
-        flashlight.SpotLight.intensity = 0.0f;
+        GameObject flashlight = GameObject.Find("/Flashlight/WhiteLight");
+        Light light = flashlight.GetComponent(typeof(Light)) as Light;
+        light.intensity = 0.0f;
         base.OnRelease();
     }
 
